b932694e5a86ca067accc0d32ce5690c8c8f1133
NeangThary <100552022+Neangthary@users.noreply.github.com>
2023-05-06 22:36:21 +0700

    final in C++ Course

--- Lesson_01.cpp
+++ Lesson_01.cpp
@@ -7,9 +7,11 @@ class AbstractEmployee {
 
 class Employee : AbstractEmployee{
 private:
-    string Name;
     string Company;
     int Age;
+protected:
+    string Name;
+    
 public:
     void setName(string name){  // Setter
         Name = name;
@@ -48,13 +50,55 @@ public:
             std::cout << Name << ", sorry no promotion for you!" << std::endl;
         
     }
+    virtual void Work() {
+        std::cout << Name << " is checking email, task backlog, performing tasks..." << std::endl;
+    }
 };
-int main(){
-    Employee employee1 = Employee("Thary", "Amazon", 32); 
-    Employee employee2 = Employee("Sok", "Google", 21);
 
-    employee1.AskForPromotion();
-    employee2.AskForPromotion();
+class Developer: public Employee {
+public:
+        string FavProgrammingLanguage;
+        // constructor
+        Developer(string name, string company, int age,string favProgrammingLanguage) 
+            :Employee(name, company, age)
+        {
+            FavProgrammingLanguage = favProgrammingLanguage;
+        }
+        void FixBug() {
+            std::cout << Name  << " fixed bug using "<< FavProgrammingLanguage  << std::endl; 
+        }
+        void Work()
+        {
+            std::cout << Name << " is writting"  <<  FavProgrammingLanguage << " code" << std::endl;
+        }
+};
 
+class Teacher:public Employee  {
+public: 
+    string Subject;
+    void PrepareLesson(){
+        std::cout << Name << " is preparing " << Subject << " lesson" << std::endl;
+    }
+    Teacher(string name, string company, int age, string subject )
+        :Employee(name, company, age)
+    {
+        Subject = subject;
+    }
+    void Work()
+    {
+        std::cout << Name << " is teacheing" << Subject  << std::endl;
+    }
+};
+
+int main(){ //The most common use of polymorphism is when a parent class reference is used to refer to a child class object.
+    
+    Developer d = Developer("Thary", "Amazon", 32, " C++");
+    Teacher t = Teacher("Sam", "True Vision", 35, " Math");
+   
+    Employee *e1 = &d; //pointer to the base class
+    Employee *e2 = &t;
+
+    e1->Work();
+    e2->Work();
 }
 
